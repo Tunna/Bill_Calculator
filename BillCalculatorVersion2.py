8f20a0ff7d874d92ab68ce8bd5b9cf3887c9f901
Noelis <you@hackbrightacademy.com>
2016-07-28 21:26:42 -0700

    Version 2

---
+++ BillCalculatorVersion2.py
@@ -0,0 +1,18 @@
+bill_amount = 0
+tip_percentage = 0
+split_number = 0
+
+def calculate_bill(bill_amt, tip_percent = 18, split = 1):
+	global bill_amount
+	global tip_percentage
+	global split_number
+	bill_amount = bill_amt
+	tip_percentage = tip_percent
+	split_number = split
+
+	return tip_amt = bill_amt * tip_percent
+	return total_bill = tip_amt + bill_amt
+	return split_amt = total_bill / split
+
+print calculate_bill(125.50, tip_percent = 18, split = 1)
+
