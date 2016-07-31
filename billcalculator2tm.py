f044a51744dd2e87f5207d7d688dadaf65f67989
Neerav Mehta <neerav.mehta@camposepc.com>
2016-07-30 22:37:28 -0700

    TM version 2

---
+++ billcalculator2tm.py
@@ -0,0 +1,17 @@
+tip_amount = 0
+total_bill = 0
+split_amount = 0
+bill_amount = 0
+
+
+def calculate_bill(bill_amt, tip_percent = 18, split_num = 1):
+	global tip_amount
+	global total_bill
+	global split_amount
+
+	return tip_amount = bill_amt * tip_percent
+	return total_bill = tip_amt + bill_amt
+	return split_amount = total_bill / split_num
+
+	print calculate_bill(100, tip_percent = 0, split = 0)
+
